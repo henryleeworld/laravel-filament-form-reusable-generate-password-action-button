9c49e0f45732f9edd98ebf15c231173200596ced
Henry Lee <henryleeworld@gmail.com>
2024-10-15 00:06:59 +0800

    feat:初始化

---
+++ lang/vendor/filament-panels/zh_TW/pages/auth/edit-profile.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    'label' => '個人資料',
+
+    'form' => [
+
+        'email' => [
+            'label' => '電子郵件',
+        ],
+
+        'name' => [
+            'label' => '姓名',
+        ],
+
+        'password' => [
+            'label' => '新的密碼',
+        ],
+
+        'password_confirmation' => [
+            'label' => '確認新的密碼',
+        ],
+
+        'actions' => [
+
+            'save' => [
+                'label' => '儲存更動',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => '已儲存',
+        ],
+
+    ],
+
+    'actions' => [
+
+        'cancel' => [
+            'label' => '取消',
+        ],
+
+    ],
+
+];
